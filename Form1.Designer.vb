b7c8def147302c38bc22052b63bdda39bd814139
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-17 11:00:24 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.BirthYear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BirthYear
+        '
+        Me.BirthYear.BackColor = System.Drawing.Color.Fuchsia
+        Me.BirthYear.Font = New System.Drawing.Font("Monotype Corsiva", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BirthYear.Location = New System.Drawing.Point(120, 68)
+        Me.BirthYear.Name = "BirthYear"
+        Me.BirthYear.Size = New System.Drawing.Size(156, 114)
+        Me.BirthYear.TabIndex = 0
+        Me.BirthYear.Text = "How old are you"
+        Me.BirthYear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BirthYear)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BirthYear As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BirthYear.Click
+        Dim YourMessage As String
+
+
+        Dim BirthYear As Integer
+        YourMessage = "Happy bday! How old are you"
+
+
+        MsgBox(YourMessage)
+        BirthYear = Val(InputBox("Enter year of birth"))
+        MsgBox("You are now " & 2023 - BirthYear & " years old")
+
+    End Sub
+End Class
